13b793426b5b108cc00ed34ea11bf83cd8bb0856
Du Luong <luongvidu@gmail.com>
2020-11-03 00:35:26 +1100

    Create Data Classes and DTOs, Also incleded AutoMapper and change appsetting files.

---
+++ BookStore-API/DTOs/AuthorDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore_API.DTOs
+{
+    /// <summary>
+    /// DTO and Data class do not directly interact
+    /// </summary>
+    public class AuthorDTO
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string Lastname { get; set; }
+        public string Bio { get; set; }
+
+        public virtual IList<BookDTO> Books { get; set; }
+    }
+}
